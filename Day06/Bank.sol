8c69b5b3ff0aba373f7ea1d5cfe1a205ed849f55
Ronin <31029205+0xronin@users.noreply.github.com>
2022-08-26 12:53:06 +0530

    Update Bank.sol

--- Day06/Bank.sol
+++ Day06/Bank.sol
@@ -1,65 +1,88 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.7;
 
+/*
+* Bank Contract: Adds balances to address and saves details of user.
+@ balanceOf mapping: maps balance to an address
+@ deposit function: deposits balances to the desired address
+@ getBalance function: retrieves and displays the balance of the address 
+@ addFund function: adds fund to own balance
+@ withdraw function: withdraws the balance from the address
+@ setUserDetails function: sets and also update details of the user 
+@ getUserDetails function: retrieves user details 
+*/
+
+error NotOwner(string message);
+error HasNotDeposited(string message);
+error AmountTooSmall(string message);
+
 contract Bank {
-    error AmountTooSmall(string error);
+
+    event FundsDeposited(address user, uint amount);
+    event ProfileUpdated(address user);
+
+    address public owner;
+    uint private constant Fee = 100;
 
     mapping (address => uint) balanceOf;
+    mapping (address => User) userDeatils;
 
-    mapping (address => customer) Kyc;
 
-    struct customer {
+    struct User {
         string name;
         uint age;
     }
 
-    uint private constant Fee = 100;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if(msg.sender != owner) {
+            revert NotOwner("You are not the owner");
+        }
+        _;
+    }
+
+    modifier hasDeposited() {
+        if(balanceOf[msg.sender] <= 0) {
+            revert HasNotDeposited("Please first deposit balance using deposit");
+        }
+        _;
+    }
 
     modifier fees(uint _amount) {
-        if(_amount <= Fee){
+        if(_amount < Fee) {
             revert AmountTooSmall("Amount must be greater than 100 wei");
         }
         _;
     }
 
-    modifier validAmount() {
-        require(balanceOf[msg.sender] > 0, "Insufficient Balance");
-        _;
+    function deposit(uint _amount) public {
+        balanceOf[msg.sender] += _amount;
+        emit FundsDeposited(msg.sender, _amount);
     }
 
-    modifier validSend(uint _amount) {
-        require(balanceOf[msg.sender] >= _amount, "Insufficient Balance");
-        _;
-    }
-
-    function addFund() public payable fees(msg.value) {
-        balanceOf[msg.sender] += msg.value;
-    }
-
-    function deposit(address payable _to, uint _amount) public validSend(_amount) {
-        _to.transfer(_amount);
-        balanceOf[msg.sender] -= _amount;
-    }
-
-    function withdraw() public validAmount {
-        address payable to = payable(msg.sender);
-        to.transfer(balanceOf[msg.sender]);
-        balanceOf[msg.sender] = 0;
-    }
-
-    function getBalance() public view returns(uint) {
+    function getBalance() public view returns(uint){
         return balanceOf[msg.sender];
     }
 
-    function setUserDetails(string memory name, uint age) public {
-        Kyc[msg.sender] = customer(
-            name,
-            age
-        );
+    function addFund(uint _amount) public hasDeposited fees(_amount){
+        balanceOf[msg.sender] += _amount;
     }
 
-    function getUserDetail() public view returns(string memory, uint) {
-        return (Kyc[msg.sender].name, Kyc[msg.sender].age);
+    function withdraw() public onlyOwner {
+        require(balanceOf[msg.sender] > 0, "Nothing to withdraw");
+        balanceOf[msg.sender] = 0;
     }
+
+    function setUserDetails(string calldata _name, uint _age) public onlyOwner{
+        userDeatils[msg.sender] = User(_name, _age);
+        emit ProfileUpdated(msg.sender);
+    }
+
+    function getUserDetails() public view returns(User memory) {
+        return userDeatils[msg.sender];
+    }
+
 }
