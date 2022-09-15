88970d4b120246b4d2c0e5967a6c6ed069137c85
Ronin <31029205+0xronin@users.noreply.github.com>
2022-09-15 12:08:22 +0530

    Update Staker.sol

--- Day11/Staker.sol
+++ Day11/Staker.sol
@@ -1,94 +1,101 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// there will be a staking period and claiming period 
-
 contract Staker {
-
-    event Stake(address indexed _staker, uint _amount);
-    event Withdraw(address indexed _withdrawer, uint _amount);
-
-    // mappings
+    // balances
     mapping(address => uint) public balances;
-    mapping(address => uint) public stakingTimestamp;
-    mapping(address => bool) public hasClaimed;
+    mapping(address => uint) public stakeTimestamp;
 
     address payable public owner;
     uint public stakePeriod = block.timestamp + 120 seconds;
     uint public claimPeriod = block.timestamp + 240 seconds;
-    uint public reward = 0.01 ether; // reward/second for staking
+    uint public reward = 0.001 ether;
 
-    // modifier for staking deadline reached
-    modifier stakingDeadlineReached(bool reached) {
-        uint timeLeft = stakeTimeLeft();
-        if(reached == true) {
-            require(timeLeft == 0, "Staking deadline not reached");
-        } else {
-            require(timeLeft > 0, "Staking deadline reached");
-        }
-        _;
-    }
+    event Stake(address indexed stakers, uint amount);
+    event Withdraw(address indexed withdrawers, uint amount);
 
-    // modifier for claiming deadline reached
-    modifier claimingDeadlineReached(bool reached) {
-        uint timeLeft = claimTimeLeft();
-        if(reached == true) {
-            require(timeLeft == 0, "Claiming deadline not reached");
-        } else {
-            require(timeLeft > 0, "Claiming deadline reached");
-        }
-        _;
-    }
-    
     constructor() {
         owner = payable(msg.sender);
     }
 
+    // modifiers 
+    // stakingDeadLineReached
+    modifier stakingDeadLineReached(bool reached) {
+        uint timeRemaining = stakePeriodLeft();
+        if(reached) {
+            require(timeRemaining == 0, "Staking Deadline Not Reached");
+        } else {
+            require(timeRemaining > 0, "Staking Deadline Reached");
+        }
+        _;
+    }
+
+    // claimDeadlineReached
+    modifier claimDeadlineReached(bool reached) {
+        uint timeRemaining = claimPeriodLeft();
+        if(reached) {
+            require(timeRemaining == 0, "Claim Deadline Not Reached");
+        } else {
+            require(timeRemaining > 0, "Claim Deadline Reached");
+        }
+        _;
+    }
+
+
     // stake function
-    function stake() public payable stakingDeadlineReached(false) {
+    /* @notice - only allowed during the staking period
+    */
+    function stake() public payable stakingDeadLineReached(false) {
         balances[msg.sender] += msg.value;
-        stakingTimestamp[msg.sender] = block.timestamp;
+        stakeTimestamp[msg.sender] = block.timestamp;
         emit Stake(msg.sender, msg.value);
     }
 
-    // withdraw function
-    function withdraw() external payable stakingDeadlineReached(true) claimingDeadlineReached(false) {
-        require(hasClaimed[msg.sender]== false, "Already Claimed");
+
+    // withdraw function 
+    /* @notice - only allowed during the claim period
+    */
+    function withdraw() public stakingDeadLineReached(true) claimDeadlineReached(false) {
+        require(balances[msg.sender] > 0, "Nothing to Withdraw");
+
         uint balanceStaked = balances[msg.sender];
-        uint claimAmount = balanceStaked + (block.timestamp - stakingTimestamp[msg.sender])* reward;
+        uint balanceWithRewards = balanceStaked + (block.timestamp - stakeTimestamp[msg.sender])*reward;
 
         balances[msg.sender] = 0;
-        (bool success, ) = msg.sender.call{value: claimAmount}("");
-        hasClaimed[msg.sender] = true;
+
+        (bool success, ) = msg.sender.call{value: balanceWithRewards}("");
         require(success, "Failed");
-        emit Withdraw(msg.sender, claimAmount);
+
+        emit Withdraw(msg.sender, balanceWithRewards);
     }
 
-    // drain function
-    function drain() public claimingDeadlineReached(true) {
-        require(msg.sender == owner, "Not the owner");
-        owner.transfer(address(this).balance);
-    }
-
-    // stake time left function
-    function stakeTimeLeft() public view returns(uint) {
-        require(msg.value > 0, "Send some ETH");
-        if(block.timestamp > stakePeriod) {
+    // stake period left
+    function stakePeriodLeft() public view returns(uint) {
+        if(block.timestamp >= stakePeriod){
             return 0;
         } else {
             return (stakePeriod - block.timestamp);
         }
     }
 
-    // claim time left function
-    function claimTimeLeft() public view returns(uint) {
-        if(block.timestamp > claimPeriod) {
+    // claim period left
+    function claimPeriodLeft() public view returns(uint) {
+        if(block.timestamp >= claimPeriod) {
             return 0;
         } else {
             return (claimPeriod - block.timestamp);
         }
     }
 
+    function drain() public payable {
+        require(block.timestamp > claimPeriod, "Cannot witdraw before Locking");
+        require(msg.sender == owner, "Not the owner");
+        owner.transfer(address(this).balance);
+    }
+
+    
     // receive function
-    receive() external payable {}
+    receive() external payable {
+
+    }
 }
